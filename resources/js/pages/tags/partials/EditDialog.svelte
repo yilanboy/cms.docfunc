9f762930717167f31c25dfd69be71783f8180659
Allen <allenjianggo@gmail.com>
2025-08-05 11:23:15 +0800

    fix: enhance EditDialog with reactive binding for tag name input

--- resources/js/pages/tags/partials/EditDialog.svelte
+++ resources/js/pages/tags/partials/EditDialog.svelte
@@ -32,6 +32,10 @@
       });
     }
   }
+
+  $effect(() => {
+    $form.name = tag ? tag.name : "";
+  });
 </script>
 
 <el-dialog id={dialogWrapperId}>
@@ -65,12 +69,7 @@
                   type="text"
                   name="new-tag-name"
                   placeholder="Type a new tag name"
-                  value={tag ? tag.name : ""}
-                  onchange={(event) => {
-                    if (event.target instanceof HTMLInputElement) {
-                      $form.name = event.target.value;
-                    }
-                  }}
+                  bind:value={$form.name}
                   class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600 sm:text-sm/6"
                 />
               </div>
