be7105ff61e2e49beb9f74591afb73fd44fb5984
allenjiang <allenjianggo@gmail.com>
2025-08-12 20:51:02 +0800

    feat: add new logo SVG and integrate as favicon in app layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+
+    {{-- Favicon --}}
+    <link rel="icon" href="{{ asset('icons/logo.svg') }}" type="image/svg+xml">
+
     @vite(['resources/css/app.css','resources/js/app.ts'])
     @inertiaHead
 </head>
