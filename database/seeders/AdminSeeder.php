8df05bde3f5a89d427d1f11cb1efa4cf21486f56
Allen <allenjianggo@gmail.com>
2025-08-12 17:47:26 +0800

    fix: use orm to create a default admin

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,10 +14,14 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         if (config('admin.name') && config('admin.email') && config('admin.password')) {
-            Admin::factory()->create([
-                'name'     => config('admin.name'),
-                'email'    => config('admin.email'),
-                'password' => bcrypt(config('admin.password')),
+            Admin::query()->create([
+                'name'              => config('admin.name'),
+                'email'             => config('admin.email'),
+                'password'          => bcrypt(config('admin.password')),
+                'email_verified_at' => now(),
+                'remember_token'    => null,
+                'created_at'        => now(),
+                'updated_at'        => now(),
             ]);
 
             return;
