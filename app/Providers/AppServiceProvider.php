a6a9bd066134f4a921fad12e630d1f6911455322
allenjiang <allenjianggo@gmail.com>
2026-01-16 00:39:07 +0800

    refactor: enforce model strict mode, use `CarbonImmutable` for dates, and align database config formatting with added options

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +28,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+        Model::shouldBeStrict();
+        Date::use(CarbonImmutable::class);
     }
 }
